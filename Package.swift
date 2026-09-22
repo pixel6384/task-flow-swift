7844cb896a10badbea77fb279c29f714f72abc52
pixel6384 <helix7342@protonmail.com>
2026-09-22 13:35:30 +0700

    Initial commit: implement core task management logic and CLI interface in Swift

---
+++ Package.swift
@@ -0,0 +1,9 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "task-flow-swift",
+    targets: [
+        .executableTarget(name: "task-flow-swift", path: "Sources")
+    ]
+)
